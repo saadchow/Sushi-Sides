5362899dd27e0abb7595dc6a9e31d9dd1b252572
Saad Chowdhury <109829705+saadchow@users.noreply.github.com>
2025-08-19 13:55:09 -0400

    Update Logo.jsx

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,8 +3,8 @@ import './Logo.css';
 export default function Logo() {
   return (
     <div className="Logo">
-      <div>SEI</div>
-      <div>CAFE</div>
+      <div>Sushi</div>
+      <div>& Sides</div>
     </div>
   );
 }
